458ad9f00d4ab3525f0b21132aadcf8db1c73611
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-08-05 17:27:45 +0900

    define model: User, UserLoginLog

--- Sources/LuftDrive.App/Data/User.cs
+++ Sources/LuftDrive.App/Data/User.cs
@@ -7,5 +7,23 @@ namespace LuftDrive.App.Data
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public Guid? Group { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PasswordSalt { get; set; }
+
+        public string Nickname { get; set; }
+        public string Affiliation { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Detail { get; set; }
+        public Guid? ProfileImage { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime? PasswordExpires { get; set; }
+        public DateTime? SuspendUntil { get; set; }
+
     }
 }
---
+++ Sources/LuftDrive.App/Data/UserLoginLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuftDrive.App.Data
+{
+    public class UserLoginLog
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        public string Address { get; set; }
+        public bool Success { get; set; }
+    }
+}
