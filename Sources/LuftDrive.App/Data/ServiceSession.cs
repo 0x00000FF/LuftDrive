37792cc7e5627af96b1b1aaa2b7187204d9bf038
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-08-06 18:56:57 +0900

    icon, and basic sketch for servicesession

--- Sources/LuftDrive.App/Data/ServiceSession.cs
+++ Sources/LuftDrive.App/Data/ServiceSession.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LuftDrive.App.Shared.Components;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +10,11 @@ namespace LuftDrive.App.Data
     public class ServiceSession : IDisposable
     {
         private bool _disposed;
+
+        private List<Window> _windows;
+        private List<Icon>   _desktopIcons;
+
+
 
         public void Dispose()
         {
