ad70ff1832de194700dce54951b10bc09fc0502a
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-08-21 16:10:48 +0900

    toggle fullscreen, out-of-screen bug fix

--- Sources/LuftDrive.App/Scripts/Interface.Window.js
+++ Sources/LuftDrive.App/Scripts/Interface.Window.js
@@ -6,6 +6,9 @@
     this.dragOffset = { x: -1, y: -1 };
     this.isDragging = false;
 
+    this.isFullScreen = false;
+    this.isMinimized = false;
+
     /*
      * Initialization codes
      */
@@ -21,40 +24,44 @@
     if (!windowTitlebarElement)
         throw "Invalid";
 
+    windowTitlebarElement.addEventListener("dblclick", e => {
+        this.isFullScreen = !this.isFullScreen;
+        windowElement.setAttribute("is-fullscreen", this.isFullScreen);
+    });
+
     windowTitlebarElement.addEventListener("mousedown", e => {
         this.isDragging = true;
 
         this.dragOffset.x = e.offsetX;
         this.dragOffset.y = e.offsetY;
-
-        console.log("[window] mousedown on titlebar, offsetX:" + this.dragOffset.x + ", offsetY:" + this.dragOffset.y);
     });
 
     windowTitlebarElement.addEventListener("mouseup", e => {
         this.isDragging = false;
-        console.log("[window] mouseup on titlebar");
     });
 
     windowTitlebarElement.addEventListener("mousemove", e => {
         if (this.isDragging) {
             windowElement.style.left = (e.clientX - this.dragOffset.x) + "px";
             windowElement.style.top = (e.clientY - this.dragOffset.y) + "px";
-
-            console.log("[window] window is moving!!");
         }
     });
 
     windowTitlebarElement.addEventListener("mouseout", e => {
         if (this.isDragging) {
-            windowElement.style.left = (e.clientX - this.dragOffset.x) + "px";
-            windowElement.style.top = (e.clientY - this.dragOffset.y) + "px";
+            const nextPosX = e.clientX - this.dragOffset.x;
+            const nextPosY = e.clientY - this.dragOffset.y;
+
+            if (e.clientX < 0 || e.clientY < 0)
+                return;
+
+            windowElement.style.left = nextPosX + "px";
+            windowElement.style.top = nextPosY + "px";
         }
     });
 
     windowElement.style.height = props.height;
     windowElement.style.width = props.width;
-
-    console.log("[window] initialized, id: " + id);
 }
 
 function CreateWindow(id) {
